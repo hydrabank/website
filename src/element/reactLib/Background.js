181f4abdbf42b0daa693322b2cb36ddec192ae09
Daniel Hyders <hello@bean.codes>
2021-10-30 20:30:20 +0100

    Update resume and site

--- src/element/reactLib/Background.js
+++ src/element/reactLib/Background.js
@@ -3,8 +3,15 @@ import "../css/Background.css";
 
 import { default as config } from "./Configuration/BackgroundConf";
 
+const DISABLED = true;
+
 function Background() {
-    return (
+    if (DISABLED === true) return (
+        <div className="background">
+
+        </div>
+    );
+    else return (
         <Particles
             className="Background"
             id="Background"
