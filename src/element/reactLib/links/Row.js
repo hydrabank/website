3ddb4835348dda0b783eb4c1607b158caad75049
Daniel Hyders <git@danny.works>
2021-10-31 03:50:39 +0200

    Change to danny.works

--- src/element/reactLib/links/Row.js
+++ src/element/reactLib/links/Row.js
@@ -6,7 +6,7 @@ import { faAddressCard, faEnvelope } from "@fortawesome/free-regular-svg-icons";
 function LinksRow() {
     return (
         <div className="Links" id="links">
-            <LinkItem href="mailto:hello@bean.codes" icon={faEnvelope} data-tip="Email" />
+            <LinkItem href="mailto:hello@danny.works" icon={faEnvelope} data-tip="Email" />
             <LinkItem href="/Resume.pdf" icon={faAddressCard} data-tip="Resume" />
             <LinkItem href="https://dsc.bio/Dannington" icon={faDiscord} data-tip="Discord (dsc.bio)" />
             <LinkItem href="https://github.com/Dannnington" icon={faGithub} data-tip="GitHub" />
