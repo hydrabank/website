da7d200ba38c620de9a82a8cd79c5ac0a7cfe95c
Daniel Hyders <git@danny.works>
2021-11-06 20:16:31 +0000

    ✨ Add initial links to pages (skills for now)

--- src/element/reactLib/links/Row.js
+++ src/element/reactLib/links/Row.js
@@ -2,6 +2,8 @@ import "../../css/Links.css";
 import LinkItem from "./Item";
 import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faAddressCard, faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPuzzlePiece } from "@fortawesome/free-solid-svg-icons";
 
 function LinksRow() {
     return (
@@ -10,6 +12,11 @@ function LinksRow() {
             <LinkItem href="/Resume.pdf" icon={faAddressCard} data-tip="Resume" />
             <LinkItem href="https://dsc.bio/Dannington" icon={faDiscord} data-tip="Discord (dsc.bio)" />
             <LinkItem href="https://github.com/Dannnington" icon={faGithub} data-tip="GitHub" />
+            <div className="LinksMember">
+            <a href="/skills" style={{color: "var(--white)"}} data-tip="Skills">
+                    <FontAwesomeIcon icon={faPuzzlePiece} />
+            </a>
+            </div>
         </div>
     );
 };
