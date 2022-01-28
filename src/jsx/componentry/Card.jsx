45af78e6dcd8ba3508cefd4d7e57511bb0f41bdb
Daniel Hyders <git@danny.works>
2022-01-28 05:57:05 -0800

    Switch cards to hook-based system in order to function with Babel/Webpack

--- src/jsx/componentry/Card.jsx
+++ src/jsx/componentry/Card.jsx
@@ -2,67 +2,65 @@
 
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 // Additional credit to Shayan on dev.to for the subcomponent format
 export { ProjectCard };
 
 const Title = () => null;
+Title.displayName = "Title";
 const Description = () => null;
+Description.displayName = "Description";
 const Image = () => null;
+Image.displayName = "Image";
 const Link = () => null;
+Link.displayName = "Link";
 const ImageDescription = () => null;
+ImageDescription.displayName = "ImageDescription";
 const ButtonText = () => null;
+ButtonText.displayName = "ButtonText";
 
 function ProjectCard(props) {
-    let processedChildren = typeof props.children.splice !== "function" ? [props.children] : props.children;
-    let properties = {};
+    var [processedChildren, setProcessedChildren] = useState(typeof props.children.splice !== "function" ? [props.children] : props.children);
+    var [title, setTitle] = useState("");
+    var [description, setDescription] = useState("");
+    var [image, setImage] = useState("");
+    var [link, setLink] = useState("");
+    var [imageDescription, setImageDescription] = useState("");
+    var [buttonText, setButtonText] = useState("");
     
-    
-    (function () {
-        let title = [];
-        let description = [];
-        let image = [];
-        let link = [];
-        let imageDescription = [];
-        let buttonText = [];
-
-        processedChildren.forEach(child => {
-            if (child.type.name === "Title") {
-                title.push(child);
-            } else if (child.type.name === "Description") {
-                description.push(child);
-            } else if (child.type.name === "Image") {
-                image.push(child);
-            } else if (child.type.name === "Link") {
-                link.push(child);
-            } else if (child.type.name === "ImageDescription") {
-                imageDescription.push(child);
-            } else if (child.type.name === "ButtonText") {
-                buttonText.push(child);
-            };
-        });
-
-        properties.title = title.length > 0 ? title[0].props.children : null;
-        properties.description = description.length > 0 ? description[0].props.children : null;
-        properties.image = image.length > 0 ? image[0].props.children : "https://mdbootstrap.com/img/new/standard/nature/184.jpg";
-        properties.link = link.length > 0 ? link[0].props.children : "#";
-        properties.imageDescription = imageDescription.length > 0 ? imageDescription[0].props.children : null;
-        properties.buttonText = buttonText.length > 0 ? buttonText[0].props.children : "View";
-    })();
+        useEffect(() => {
+            processedChildren.forEach(child => { 
+                console.log(child) 
+                if (child.type.displayName === "Title") {
+                    if (title === "") setTitle(child.props.children);
+                } else if (child.type.displayName === "Description") {
+                    if (description === "") setDescription(child.props.children);
+                } else if (child.type.displayName === "Image") {
+                    if (image === "") setImage(child.props.children);
+                } else if (child.type.displayName === "Link") {
+                    if (link === "") setLink(child.props.children);
+                } else if (child.type.displayName === "ImageDescription") {
+                    if (imageDescription === "") setImageDescription(child.props.children);
+                } else if (child.type.displayName === "ButtonText") {
+                    if (buttonText === "") setButtonText(child.props.children);
+                };
+            });
+        }, [processedChildren]);
         
     return (
         <div>
             <div className="flex justify-center items-start rounded-lg">
                 <div className="shadow-xl rounded-3xl bg-gray-700 max-w-sm">
-                    <a target="_blank" rel="noreferrer noopener" href={properties.link}>
-                        <img className="rounded-t-3xl" src={properties.image} alt={properties.imageDescription}/>
+                    <a target="_blank" rel="noreferrer noopener" href={link}>
+                        <img className="rounded-t-3xl" src={image} alt={imageDescription}/>
                     </a>
                     <div className="p-6">
-                        <h1 className="text-gray-100 text-xl font-bold mb-2 font-BreezeHeader">{properties.title}</h1>
+                        <h1 className="text-gray-100 text-xl font-bold mb-2 font-BreezeHeader">{title}</h1>
                         <p className="text-gray-200 text-base mb-4 font-BreezeText">
-                            {properties.description}
+                            {description}
                         </p>
-                        <a target="_blank" rel="noreferrer noopener" type="button" className="font-BreezeText inline-block px-6 py-2.5 bg-cyan-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-cyan-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-cyan-700 active:shadow-lg transition duration-150 ease-in-out" href={properties.link}>{properties.buttonText} <FontAwesomeIcon className="ml-2" icon={faExternalLinkAlt} /></a>
+                        <a target="_blank" rel="noreferrer noopener" type="button" className="font-BreezeText inline-block px-6 py-2.5 bg-cyan-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-cyan-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-cyan-700 active:shadow-lg transition duration-150 ease-in-out" href={link}>{buttonText} <FontAwesomeIcon className="ml-2" icon={faExternalLinkAlt} /></a>
                     </div>
                 </div>
             </div>
