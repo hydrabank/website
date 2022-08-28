1397696fb884ab84bebfc9a85f30c0b84d2864c3
Tomas Smith <personal@tomas.systems>
2022-08-28 19:01:45 +0100

    Update Nav.jsx

--- src/jsx/componentry/Nav.jsx
+++ src/jsx/componentry/Nav.jsx
@@ -12,7 +12,7 @@ import { Tooltip } from '@mui/material';
 const navigation = [
   { name: 'Home', href: '/', current: false, external: false },
   { name: 'Services', href: '/services', current: false, disabled: false },
-  { name: 'Exposé', href: 'https://cumsock.gay', current: false, disabled: true }
+  { name: 'Hosting', href: 'https://volteric.com', current: false, disabled: false }
 ];
 
 function classNames(...classes) {
